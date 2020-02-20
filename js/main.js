13f921de3f0eaf9e075423cf0b36fb14d0916008
Tatiana Ivanova <komarivanova@gmail.com>
2020-02-21 00:54:53 +0300

    +video.css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,54 @@
-$(document).ready(function () {
 
-    $('.carousel').carousel({hAlign:'center', 
+$(document).ready(function () {
+          
+    $(".nav-bar a").click(function(){
+        $("body,html").animate({
+        scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+        
+    });
+                 
+ 
+    $('#responsive').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+    {
+    breakpoint: 1024,
+    settings: {
+    slidesToShow:2,
+    slidesToScroll:1,
+    infinite: true,
+    dots: false
+    }
+    },
+    {
+    breakpoint: 600,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false
+    }
+    }
+    ]
+    });
+
+
+
+    $('#carousel').carousel({hAlign:'center', 
                                 vAlign:'center', 
                                 hMargin:0.56, 
                                 vMargin:0.4, 
@@ -33,46 +81,3 @@ $(document).ready(function () {
         });                      
        
     });
-    
-          
-$(".nav-bar a").click(function(){
-    $("body,html").animate({
-     scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-    
- });
-                 
- 
-$('#responsive').slick({
-dots: false,
-infinite: true,
-speed: 300,
-slidesToShow: 3,
-slidesToScroll: 1,
-responsive: [
-{
-breakpoint: 1024,
-settings: {
-slidesToShow: 2,
-slidesToScroll:1,
-infinite: true,
-dots: false
-}
-},
-{
-breakpoint: 600,
-settings: {
-slidesToShow: 1,
-slidesToScroll: 1
-}
-},
-{
-breakpoint: 480,
-settings: {
-slidesToShow: 1,
-slidesToScroll: 1
-}
-}
-]
-});
-
