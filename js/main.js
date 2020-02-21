2eae156df35b6f880cc5aeb7242186b6227cbdbf
Tatiana Ivanova <komarivanova@gmail.com>
2020-02-22 02:03:56 +0300

    +new owl carousel for reviews + reviews.css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
+$(".nav-bar a").click(function(){
+    $("body,html").animate({
+    scrollTop:$("#" + $(this).data('value')).offset().top
+    },1000)
+    
+});
+    
+$(document).ready(function(){
 
-$(document).ready(function () {
-          
-    $(".nav-bar a").click(function(){
-        $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-        },1000)
+    $('.carousel').carousel({
+    
+        carouselWidth:930,
+        carouselHeight:330,
+        directionNav:true, 
+        shadow:true,
+        buttonNav:'bullets',
+        hAlign:'center', 
+        vAlign:'center',
+        hMargin:0.56, 
+        vMargin:0.4, 
+        frontWidth:350, 
+        frontHeight:600,
+        slidesPerScroll:7,
+        backZoom:0.8, 
         
     });
-                 
- 
-    $('#responsive').slick({
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        stagePadding:30,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+
+
+});
+   /*$('#responsive').slick({
     dots: false,
     infinite: true,
     speed: 300,
@@ -45,39 +84,4 @@ $(document).ready(function () {
     }
     ]
     });
-
-
-
-    $('#carousel').carousel({hAlign:'center', 
-                                vAlign:'center', 
-                                hMargin:0.56, 
-                                vMargin:0.4, 
-                                frontWidth:350, 
-                                frontHeight:600, 
-                                carouselWidth:930, 
-                                carouselHeight:330, 
-                                left:0, 
-                                right:0, 
-                                top:0, 
-                                bottom:0, 
-                                backZoom:0.8, 
-                                slidesPerScroll:7, 
-                                speed:500, 
-                                buttonNav:'bullets', 
-                                directionNav:true,
-                                autoplay:true, 
-                                autoplayInterval:3000, 
-                                pauseOnHover:true, 
-                                mouse:true, 
-                                shadow:true, 
-                                reflection:false, 
-                                reflectionHeight:0.2, 
-                                reflectionOpacity:0.5, 
-                                reflectionColor:'0,0,0',
-                                description:false, descriptionContainer:'.description',
-                                backOpacity:1,
-                                before: function(carousel){}, 
-                                after: function(carousel){}
-        });                      
-       
-    });
+    */
