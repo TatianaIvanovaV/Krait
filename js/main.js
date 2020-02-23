e86e45842f01119978e004ac8c7236433e1945b4
Tatiana Ivanova <komarivanova@gmail.com>
2020-02-23 04:10:03 +0300

    + video.css

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(".nav-bar a").click(function(){
     },1000)
     
 });
-    
+
+
+
 $(document).ready(function(){
 
     $('.carousel').carousel({
@@ -48,40 +50,6 @@ $(document).ready(function(){
 
 
 });
-   /*$('#responsive').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-    {
-    breakpoint: 1024,
-    settings: {
-    slidesToShow:2,
-    slidesToScroll:1,
-    infinite: true,
-    dots: false
-    }
-    },
-    {
-    breakpoint: 600,
-    settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: false
-    }
-    },
-    {
-    breakpoint: 480,
-    settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: false
-    }
-    }
-    ]
-    });
-    */
+
+ 
+
